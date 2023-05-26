a699058e716ce940c10c5f53b457d8102086cf42
azotov <zotovprog@gmail.com>
2023-05-27 00:47:36 +0300

    Updates

---
+++ static/chunks/app/page-e560006cb2763b3a.js
@@ -0,0 +1,9 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931],{8969:function(e,r,n){Promise.resolve().then(n.bind(n,1761))},1761:function(e,r,n){"use strict";n.r(r),n.d(r,{default:function(){return i}});var t=n(9268);let o=()=>{var e;return null===(e=window.Telegram)||void 0===e?void 0:e.WebApp};function i(){var e,r;return console.log("here",null===(e=o())||void 0===e?void 0:e.platform),(0,t.jsx)("main",{className:"flex min-h-screen flex-col items-center justify-between p-24",children:(0,t.jsx)("p",{children:null===(r=o())||void 0===r?void 0:r.platform})})}},3177:function(e,r,n){"use strict";/**
+ * @license React
+ * react-jsx-runtime.production.min.js
+ *
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */var t=n(6006),o=Symbol.for("react.element"),i=(Symbol.for("react.fragment"),Object.prototype.hasOwnProperty),l=t.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,u={key:!0,ref:!0,__self:!0,__source:!0};r.jsx=function(e,r,n){var t,f={},s=null,c=null;for(t in void 0!==n&&(s=""+n),void 0!==r.key&&(s=""+r.key),void 0!==r.ref&&(c=r.ref),r)i.call(r,t)&&!u.hasOwnProperty(t)&&(f[t]=r[t]);if(e&&e.defaultProps)for(t in r=e.defaultProps)void 0===f[t]&&(f[t]=r[t]);return{$$typeof:o,type:e,key:s,ref:c,props:f,_owner:l.current}}},9268:function(e,r,n){"use strict";e.exports=n(3177)}},function(e){e.O(0,[667,488,744],function(){return e(e.s=8969)}),_N_E=e.O()}]);
